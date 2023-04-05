76b990b3e965ca93872d7a2f48366fd83c39e683
tuk91541 <tuk91541@temple.edu>
2023-04-05 14:22:19 -0400

    commit

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -3,9 +3,19 @@ package edu.temple.viewpagerinclasssctivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize the TextFragment with a parameter
+        val textFragment = TextFragment.newInstance("Your parameter here")
+
+        // Add the TextFragment to the container in the activity's layout
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, textFragment)
+            commit()
+        }
     }
 }
