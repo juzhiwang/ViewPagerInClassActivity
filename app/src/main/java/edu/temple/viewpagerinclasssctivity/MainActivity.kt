bce7d865ef2b894f4650b2b8c84a5880c3f37bd4
tuk91541 <tuk91541@temple.edu>
2023-04-06 12:15:45 -0400

    commit

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,20 +2,52 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.TextView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+    private var numberOfPages = 0
+    private var currentNumber = 1
+
+    private val newButton: Button by lazy {
+        findViewById(R.id.newButton)
+    }
+
+    private val textViewNumber: TextView by lazy {
+        findViewById(R.id.textView_number)
+    }
+
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.textView_number)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize the TextFragment with a parameter
-        val textFragment = TextFragment.newInstance("Your parameter here")
+        newButton.setOnClickListener {
+            currentNumber++
+            textViewNumber.text = currentNumber.toString()
 
-        // Add the TextFragment to the container in the activity's layout
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment_container, textFragment)
-            commit()
+            numberOfPages++
+            viewPager.adapter?.notifyItemInserted(numberOfPages)
         }
+
+        viewPager.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount() = numberOfPages
+
+            override fun createFragment(position: Int) = TextFragment.newInstance(position.toString())
+        }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                currentNumber = position + 2
+                textViewNumber.text = currentNumber.toString()
+            }
+        })
     }
 }
+
